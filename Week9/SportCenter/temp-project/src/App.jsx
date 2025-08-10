8fe99df8de9b93659ffa3bd7109c22c4d36a781c
pelinbasaslan <pelin.basaslan@gmail.com>
2025-08-11 00:00:26 +0300

    .

--- Week9/SportCenter/temp-project/src/App.jsx
+++ Week9/SportCenter/temp-project/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import React, { useEffect } from "react";
+import { useNavbarScroll, useHamburgerMenu, calculateBMI } from "./Hooks/useAppFunction";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import Header from './Header';
+import HomeBoxes from './HomeBoxes';
+import Classes from './Classes';
+import Trainer from './Trainer';
+import Purchase from './Purchase';
+import Review from './Review';
+import Contact from './Contact';
+import Footer from './Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useNavbarScroll();
+  useHamburgerMenu();
 
+  useEffect(() => {
+    calculateBMI();
+  }, []);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'>
+        <Header />
+        <HomeBoxes />
+        <Classes />
+        <Trainer />
+        <Purchase />
+        <Review />
+        <Contact />
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
